2ae98eff4bdea070171b67ba3d4a2b89246e9119
Farhan Kabir <autosparkbd@gmail.com>
2026-03-01 19:10:15 +0600

    Updates

---
+++ assets/js/Card-CvIg21is.js
@@ -0,0 +1,1 @@
+import{j as e}from"./react-vendor-ccQPk2iu.js";import{a as t}from"./index-BmmOPWw1.js";const i=({children:r,className:o="",hover:a=!1})=>{const{theme:s}=t();return e.jsx("div",{className:`${s==="dark"?"bg-gray-800 border border-gray-700":"bg-white"} rounded-lg shadow-md ${a?"hover:shadow-xl transition-shadow duration-300":""} ${o}`,children:r})};export{i as C};
