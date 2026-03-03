f85c73ecd731b3277bfb07469c8746c7c3624052
Farhan Kabir <autosparkbd@gmail.com>
2026-03-04 00:53:49 +0600

    Updates

---
+++ assets/js/supabase-B_CbBTUX.js
@@ -0,0 +1,1 @@
+import{c as e}from"./supabase-DZxFKcQQ.js";const t=e("https://dummy.supabase.co","dummy-key");export{t as supabase};
--- assets/js/supabase-D9xwlrWw.js
+++
@@ -1,1 +0,0 @@
-import{c as s}from"./supabase-DZxFKcQQ.js";const e=s("https://dummy.supabase.co","dummy-key");export{e as s};
