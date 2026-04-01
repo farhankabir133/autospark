427fc801a7b6008ef4be92f0e90dfafa68022227
Farhan Kabir <autosparkbd@gmail.com>
2026-04-01 17:20:20 +0600

    fix: correct init() method signature in sslcommerz declaration - accept data parameter

--- next-sslcommerz/sslcommerz.d.ts
+++ next-sslcommerz/sslcommerz.d.ts
@@ -1,7 +1,7 @@
 declare module 'sslcommerz-lts' {
   class SSLCommerzPayment {
     constructor(store_id: string, store_passwd: string, is_live: boolean);
-    init(): Record<string, unknown>;
+    init(data: Record<string, unknown>): Promise<Record<string, unknown>>;
     validate(data: Record<string, unknown>): Promise<any>;
   }
   export default SSLCommerzPayment;
