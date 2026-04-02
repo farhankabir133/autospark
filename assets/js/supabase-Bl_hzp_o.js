a19e7aa45c4091d2c245692ac539184db8cdb566
Farhan Kabir <autosparkbd@gmail.com>
2026-04-02 19:23:43 +0600

    Updates

--- assets/js/supabase-Bl_hzp_o.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./supabase-DnPY-mAy.js";const t=e("https://dummy.supabase.co","dummy-key");export{t as supabase};
---
+++ assets/js/supabase-DS49eHmu.js
@@ -0,0 +1,1 @@
+import{c as s}from"./supabase-BQEU2DLB.js";const a="https://hcdwfxnvmvvkbpeshbqk.supabase.co",e="sb_publishable_o4V4NsBTa1omeSCyl8GuuA_UppA17sl",o=s(a,e);export{o as supabase};
